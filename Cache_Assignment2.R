474448dbf26d84e30484da337a892b8a71b1be6d
yashaf2024 <yashaarhafizka@gmail.com>
2024-12-13 17:02:19 -0500

    Update Cache_Assignment2.R

--- Cache_Assignment2.R
+++ Cache_Assignment2.R
@@ -26,22 +26,3 @@ cacheSolve <- function(x, ...) {
         x$setinverse(m)
         m
 }
-
-# Testing
-# Define a test matrix
-test_matrix <- matrix(c(2, 1, 1, 3), nrow = 2, ncol = 2)
-
-# Create the special "matrix" object
-cacheMatrix <- makeCacheMatrix(test_matrix)
-
-# Compute the inverse and cache it
-inverse1 <- cacheSolve(cacheMatrix)
-print(inverse1)
-
-# Test caching by calling cacheSolve again
-inverse2 <- cacheSolve(cacheMatrix)
-print(inverse2)
-
-# Validate the inverse by multiplying the matrix by its inverse
-identity_matrix <- test_matrix %*% inverse1
-print(identity_matrix)  # Should be close to the identity matrix
